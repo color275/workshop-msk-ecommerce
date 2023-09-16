5ed8e24b058205019e8012a9ad7532a4bd8c0437
Your Name <color275@gmail.com>
2023-09-16 20:41:46 +0900

    modify

--- insert.py
+++ insert.py
@@ -20,7 +20,7 @@ def generate_data():
     
     return (cust_id, prd_id, order_cnt, order_price, order_dt, last_update_time, promo_id)
 
-batch_size = 100
+batch_size = 10
 while True:
     data_to_insert = [generate_data() for _ in range(batch_size)]
     
@@ -29,6 +29,7 @@ while True:
     
     cursor.executemany(sql, data_to_insert)
     db.commit()
+    print("# insert {}row...".format(batch_size))
     
     time.sleep(1)
 
